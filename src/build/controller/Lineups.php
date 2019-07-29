f2e66e897b39c654afa365fdfe1a8eb228958dc9
xml-peter <xml.p.ombao@gmail.com>
2019-07-29 20:54:02 +0800

    player alias crawl

---
+++ src/build/controller/Lineups.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace Scraper\Build\Controller;
+
+use Scraper\Kernel\Interfaces\Database;
+use Scraper\Kernel\Crawler\Crawler;
+use Scraper\Kernel\Interfaces\Controller;
+use Scraper\Build\Pattern\Lineups as LineupsPattern;
+
+class Lineups implements Controller {
+
+    
+    private $database;
+    private $event_ids;
+    private $event_id;
+    private $status;
+    private $start_date;
+
+
+    public function __construct(Database $database, $param = []) {
+        $this->database = $database;
+        /**
+         * Check if the param event_id exists on params
+         */
+        if (@array_key_exists('event_id', $param)) {
+            $this->event_ids = $param['event_id'];
+        }
+        $this->start_date = (!empty($param['start_date'])) ? $param['start_date'] : '';
+
+        /**
+         * Start standings
+         */
+        $this->lineups();
+    }
+
+
+
+    /**
+     * Livescore starting function for parsing/crawling
+     */
+    public  function lineups() {
+        $types = [
+            '#match-summary',
+            '#match-statistics;0',
+            '#lineups;1'
+        ];
+        $type = '#lineups;1';
+        
+        $matches = $this->getMatches();
+        echo "> To Scrape: " . count($matches) . NL;
+        /**
+         * Loop return matches
+         */
+        foreach ($matches as $key => $match) {
+            /**
+             * Pre-defined vars
+             */
+
+            /**
+             * Loop types
+             */
+            $ids = array();
+            $ids['event_id'] = $match->event_id;
+            $ids['home_team_id'] = $match->home_team_id;
+            $ids['away_team_id'] = $match->away_team_id;
+            $new_url = $match->flashscore_link;
+            $new_url = str_replace($types, '', $new_url);
+            $new_url = $new_url . $type;
+            $base_name = camel_case(preg_replace('/[0-9]+/', '', str_slug($type)));
+            $file = FILE_PATH . $base_name . '.html';
+
+            // Start Crawling
+            $crawler = new Crawler($new_url);
+            $crawler->standingsCrawl($file);
+            // Start parsing, methods below: dynamically called base on type
+            $this->lineups_content($file, $ids);
+        }
+    }
+
+
+    protected function getMatches() {
+        if ($this->event_ids) {
+            $event_ids = $this->event_ids;
+            $sql = "
+            SELECT 
+            e.`id` AS event_id,
+            home_ep.`participantFK` AS home_team_id,
+            away_ep.`participantFK` AS away_team_id,
+            fs.`flashscore_link` AS flashscore_link
+            FROM `event` e
+            JOIN flashscore_source fs ON e.`id` = fs.`eventFK`
+            JOIN event_participants home_ep ON e.`id` = home_ep.`eventFK` AND home_ep.`del` = 'no' AND home_ep.`number` = 1
+            JOIN event_participants away_ep ON e.`id` = away_ep.`eventFK` AND away_ep.`del` = 'no' AND away_ep.`number` = 2
+            WHERE fs.`flashscore_link` != ''
+            AND fs.`flashscore_link` LIKE '%flashscore.com%'
+            AND e.`del` = 'no'
+            AND e.`status_type` NOT IN ('deleted', 'notstarted', 'inprogress')
+            AND e.`id` IN ($event_ids)
+            GROUP BY e.`id`
+            ";
+        }else{
+            if(!empty($this->start_date)){
+                $yesterday = date('Y-m-d H:i:s', strtotime($this->start_date));
+            }else{
+                $yesterday = date('Y-m-d H:i:s', strtotime('- 4 hours'));
+            }
+            
+            $sql = "
+            SELECT 
+            e.`id` AS event_id,
+            home_ep.`participantFK` AS home_team_id,
+            away_ep.`participantFK` AS away_team_id,
+            fs.`flashscore_link` AS flashscore_link
+            FROM `event` e
+            JOIN flashscore_source fs ON e.`id` = fs.`eventFK`
+            JOIN event_participants home_ep ON e.`id` = home_ep.`eventFK` AND home_ep.`del` = 'no' AND home_ep.`number` = 1
+            JOIN event_participants away_ep ON e.`id` = away_ep.`eventFK` AND away_ep.`del` = 'no' AND away_ep.`number` = 2
+            WHERE fs.`flashscore_link` != ''
+            AND fs.`flashscore_link` LIKE '%flashscore.com%'
+            AND e.`del` = 'no'
+            AND e.`status_type` NOT IN ('deleted', 'notstarted', 'inprogress')
+            GROUP BY e.`id`
+            LIMIT 1
+            ";
+        }
+        $matches = $this->database->query($sql);
+        return $matches;
+    }
+
+
+
+    /**
+     * @func call match events parser 
+     */
+    protected function lineups_content($file, $ids) {
+        /**
+         * Parse html file to data
+         */
+        $parser = new LineupsPattern($ids, $file, $this->database);
+        /**
+         * Get all events parsed from source
+         */
+        $lineups = $parser->lineups();
+        // dump($lineups);
+
+        /**
+         * Start inserting data
+         */
+        // if (count($standings) > 0) {
+        //     /**
+        //      * Update standings
+        //      */
+        //     $parser->updateStandings($standings);
+        //     /**
+        //      * Update adjustments
+        //      */
+        //     $parser->updateAdjustments($standings);
+        // }
+    }
+}
---
+++ src/build/patterns/Lineups.php
@@ -0,0 +1,351 @@
+<?php
+/**
+ * Pattern Lineups
+ */
+namespace Scraper\Build\Pattern;
+
+use \DOMDocument;
+use \DomXPath;
+
+use Scraper\Kernel\Interfaces\Database as DatabaseInterface;
+
+
+/** 
+* @class Parser
+* Parsing of the HTML to data
+*/
+class Lineups {
+    
+    public $event_id = 0;
+    public $away_str11_list = array();
+    public $home_str11_list = array();
+    public $events_ls = array();
+    public $my_file = "";
+    public $time_holder=0;
+    private $database;
+    public $ids = array();
+    
+    public function __construct($ids, $fle, $db)
+    {
+        $this->ids = $ids;
+        $this->my_file = $fle;
+        $this->database = $db;
+    }
+
+    public function lineups(){
+        $database = $this->database;
+        $data = file_get_contents( $this->my_file );
+        $return = array(
+            'home' => array(
+                'starting' => array(),
+                'substitutes' => array(),
+                'missing' => array()
+            ),
+            'away' => array(
+                'starting' => array(),
+                'substitutes' => array(),
+                'missing' => array()
+            )
+        );
+
+        libxml_use_internal_errors(true);
+        $dom = new DOMDocument();
+        $dom->loadHTML($data);
+        $dom->encoding = 'UTF-8';
+        $finder = new DomXPath($dom);
+        $wrappers = array(
+            'lineups-wrapper',
+            'missing-players-wrapper'
+        );
+        foreach($wrappers as $wrapper){
+            $divs = $finder->query("//*[contains(@class, '" . $wrapper . "')]");
+            $div = $divs->item(0);
+            $trs = $div->getElementsByTagName('tr');
+            foreach($trs as $tr){
+                $tds = $tr->getElementsByTagName('td');
+                foreach($tds as $td){
+                    $td_class = explode(' ', $td->getAttribute('class'));
+                    $homeAway = '';
+                    if(in_array('fl', $td_class)){
+                        $homeAway = 'home';
+                    }else if(in_array('fr', $td_class)){
+                        $homeAway = 'away';
+                    }
+
+                    if(!empty($homeAway)){
+                        $a_tags = $td->getElementsByTagName('a');
+                        foreach($a_tags as $a_tag){
+                            $fs_onclick = $a_tag->getAttribute('onclick');
+                            if(!empty($fs_onclick)){
+                                $fs_onclick = str_replace("window.open('/player/", '', str_replace("/'); return false;", '', $fs_onclick));
+                                $player_onclick = explode('/', $fs_onclick);
+
+                                $team_id = $this->ids[$homeAway . '_team_id'];
+
+                                $player_shortname = $a_tag->textContent;
+                                $spans = $a_tag->getElementsByTagName('span');
+                                foreach($spans as $span){
+                                    $span_text = $span->textContent;
+                                    $player_shortname = str_replace($span_text, '', $player_shortname);
+                                }
+
+                                $player_params = array(
+                                  'player_fullname' => $player_onclick[0],
+                                  'player_shortname' => $player_shortname,
+                                  'team_id' => $team_id,
+                                  'shirt_number' => null,
+                                  'fs_id' => $player_onclick[1]
+                                );
+
+                                $player = $this->playerFinder($player_params, $this->ids['event_id']);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return $return;
+    }
+
+    public function playerFinder($player_data, $event_id)
+    {
+        $database = $this->database;
+
+        $result = 0;
+        $alias_result = 0;
+        $result_name = '';
+        $result_source = '';
+
+        $res = $database->query(
+            "SELECT * FROM flashscore_alias " .
+            "WHERE fs_alias = '" . $player_data['player_fullname'] . "' " .
+            "AND participant_type = 'athlete' " .
+            "AND fs_id = '" . $player_data['fs_id'] . "'"
+        );
+
+
+        if (count($res) == 1) {
+            $flashscore_alias_exists = $res[0];
+        }
+
+        if(!empty($flashscore_alias_exists) && $flashscore_alias_exists->participantFK > 0){
+            $result = $flashscore_alias_exists->participantFK;
+            $result_source = 'flashscore_alias';
+        }
+
+        if(empty($result)){
+          $player_name_clean = html_entity_decode(str_replace("&nbsp;", "", htmlentities($player_data['player_shortname'])));
+          $player_name_clean = trim(preg_replace("/\([^)]+\)/", '', $player_name_clean));
+
+          $player_name_clean = str_replace(' (C)', '', $player_name_clean);
+
+          $player_name_clean = str_replace(' (G)', '', $player_name_clean);
+
+          $explode = explode(' ', $player_name_clean);
+          $explode_length = count($explode);
+
+          $player_where = array();
+
+          $surname = array();
+          $initial = array();
+          for ($i = 0; $i < $explode_length; $i++) {
+            $if_initial = strpos($explode[$i], '.');
+            $each_length = strlen($explode[$i]);
+            if($if_initial !== false && $if_initial == ($each_length - 1)){
+              $initial[] = str_replace(".", "", $explode[$i]);
+
+              if ($i < ($explode_length - 1)) {
+                $if_initial = strpos($explode[$explode_length - 1], '.');
+                $each_length = strlen($explode[$explode_length - 1]);
+                if($if_initial !== false && $if_initial == ($each_length - 1)){
+                  $initial[] = str_replace(".", "", $explode[$explode_length - 1]);
+                }
+              }
+              break;
+            }elseif($if_initial === false){
+              $surname[] = $explode[$i];
+            }
+          }
+
+          $get_surname = implode(' ', $surname);
+          $laravel_str_slug = str_slug($get_surname, '-');
+          $explode_fullname = explode('-', $player_data['player_fullname']);
+
+          if (!empty($initial)) {
+            for ($i = 0; $i < count($initial); $i++) {
+              $get_firstname = $player_data['player_fullname'];
+              $get_firstname = str_replace($laravel_str_slug, '', $get_firstname);
+              $get_firstname = trim($get_firstname, '-');
+              $get_firstname_arr = explode('-', $get_firstname);
+              $get_firstname = '';
+              for($k = 0; $k < count($get_firstname_arr); $k++){
+                $this_firstname = substr($get_firstname_arr[$k], 0, strlen($initial[$i]));
+                if($this_firstname == strtolower($initial[$i])){
+                  $get_firstname = $get_firstname_arr[$k];
+                  break;
+                }
+              }
+              if(!empty($get_firstname)){
+                $row = array();
+                $row[] = array('name', 'like', '%' . $get_surname);
+                $row[] = array('name', 'like', $get_firstname. '%');
+                $player_where[] = $row;
+
+                $row = array();
+                $row[] = array('name', 'like', $get_surname . '%');
+                $row[] = array('name', 'like', '%' . $get_firstname);
+                $player_where[] = $row;
+              }
+            }
+          }else{
+            if(count($surname) == 1 && count($explode_fullname) == 2){
+              $row = array();
+              $row[] = array('name', 'like', '%' . $explode_fullname[0]);
+              $row[] = array('name', 'like', $explode_fullname[1] . '%');
+              $player_where[] = $row;
+
+              $row = array();
+              $row[] = array('name', 'like', $explode_fullname[0] . '%');
+              $row[] = array('name', 'like', '%' . $explode_fullname[1]);
+              $player_where[] = $row;
+            }elseif(count($surname) == 2){
+              $row = array();
+              $row[] = array('name', 'like', '%' . $surname[0]);
+              $row[] = array('name', 'like', $surname[1] . '%');
+              $player_where[] = $row;
+
+              $row = array();
+              $row[] = array('name', 'like', $surname[0] . '%');
+              $row[] = array('name', 'like', '%' . $surname[1]);
+              $player_where[] = $row;
+            }elseif(count($surname) > 2){
+              $row = array();
+              $row[] = array('name', '=', '%' . $get_surname);
+              $player_where[] = $row;
+            }
+          }
+        }
+
+        if(empty($result) && empty($initial) && count($surname) == 1){
+            $surname = current($surname);
+            $res = $database->query(
+                "SELECT participant.`id`, participant.`name` " . 
+                "FROM object_participants " .
+                "JOIN participant ON participantFK = participant.`id` " . 
+                "WHERE objectFK = " . $player_data['team_id'] . " " .
+                "AND participant_type = 'athlete' " .
+                "AND object_participants.active = 'yes' " .
+                "AND participant.name = \"" . $surname . "\" " .
+                "GROUP BY participant.id"
+            );
+
+            $query_count = count($res);
+
+            if ($query_count == 1) {
+                foreach ($res as $rw) {
+                    $result = $rw->id;
+                    $result_name = $rw->name;
+                }
+                
+                $result_source = 'object_participants (2.1)';
+            }
+        }
+
+        if(empty($result) && !empty($player_where)){
+            $res_string = "SELECT participant.`id`, participant.`name` " . 
+            "FROM object_participants " . 
+            "JOIN participant ON participantFK = participant.`id` " . 
+            "WHERE objectFK = " . $player_data['team_id'] . " " .
+            "AND participant_type = 'athlete' " .
+            "AND object_participants.active = 'yes' " . 
+            "AND (";
+            for ($j = 0; $j < count($player_where); $j++) {
+                if($j != 0){
+                    $res_string .= " OR ";
+                }
+                $res_string .= "(";
+                $res_string .= $player_where[$j][0][0] . " " . $player_where[$j][0][1] . " \"" . $player_where[$j][0][2] . "\"";
+
+                if (!empty($player_where[$j][1])) {
+                    $res_string .= " AND ";
+                    $res_string .= $player_where[$j][1][0] . " " . $player_where[$j][1][1] . " \"" .$player_where[$j][1][2] . "\"";
+                }
+
+                $res_string .= ")";
+            }
+            $res_string .= ") " .
+            "GROUP BY participant.id";
+
+            $res = $database->query($res_string);
+
+            $query_count = count($res);
+
+            if ($query_count == 1) {
+                foreach ($res as $rw) {
+                    $result = $rw->id;
+                    $result_name = $rw->name;
+                }
+               
+                $result_source = 'object_participants (2.2)';
+            }
+        }
+
+        if (empty($result) && !empty($player_where)) {
+            $res_string = "SELECT id, name " . 
+            "FROM participant " . 
+            "WHERE type = 'athlete' " .
+            "AND (";
+            for ($j = 0; $j < count($player_where); $j++) {
+                if($j != 0){
+                    $res_string .= " OR ";
+                }
+                $res_string .= "(";
+                $res_string .= $player_where[$j][0][0] . " " . $player_where[$j][0][1] . " \"" . $player_where[$j][0][2] . "\"";
+
+                if (!empty($player_where[$j][1])) {
+                    $res_string .= " AND ";
+                    $res_string .= $player_where[$j][1][0] . " " . $player_where[$j][1][1] . " \"" .$player_where[$j][1][2] . "\"";
+                }
+
+                $res_string .= ")";
+            }
+            $res_string .= ")";
+
+            $res = $database->query($res_string);
+
+            $query_count = count($res);
+
+            if ($query_count == 1) {
+                foreach ($res as $rw) {
+                    $result = $rw->id;
+                    $result_name = $rw->name;
+                }
+                
+                $result_source = 'participant';
+            }
+        }
+
+        if (!empty($flashscore_alias_exists)) {
+          $alias_result = $flashscore_alias_exists->id;
+          if(!empty($result) && $flashscore_alias_exists->participantFK == 0){
+            $database->query("UPDATE flashscore_alias SET participantFK = $result WHERE id = $alias_result");
+          }
+        }else{
+            $alias_result = $database->instance()->table('flashscore_alias')->insertGetId([
+                'participantFK' => $result,
+                'event_id' => $event_id,
+                'team_id' => $player_data['team_id'],
+                'fs_alias' => $player_data['player_fullname'],
+                'participant_type' => 'athlete',
+                'fs_id' => $player_data['fs_id']
+            ]);
+        }
+
+        return array(
+          'participant_id' => $result,
+          'flashscore_alias_id' => $alias_result
+        );
+    }
+
+}
